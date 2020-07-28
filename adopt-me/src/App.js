6a7d1f11f69bc5b14656cd89ad1a397a4b71e80e
John Reiland <reiland.shanghai@gmail.com>
2020-07-28 19:57:36 -0400

    "Add App.js, index.html, style.css"

---
+++ adopt-me/src/App.js
@@ -0,0 +1,32 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    [
+      React.createElement(Pet, {
+        name: "Harry",
+        animal: "Bird",
+        breed: "Parakeet",
+      }),
+      React.createElement(Pet, {
+        name: "Elizabeth",
+        animal: "Bird",
+        breed: "Parakeet",
+      }),
+      React.createElement(Pet, {
+        name: "Gizmo",
+        animal: "Rodent",
+        breed: "Hampster",
+      }),
+    ],
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
